ecc1479da80a0ecc40e871658f076dc692e2d8d1
olwang <31269214+olwang@users.noreply.github.com>
2026-05-06 08:22:10 -0700

    Revert "Add Pretext-assisted client text hints demo"
    This reverts commit e6a4a4edbe733cd76953f9cb9356388140958570.

--- scripts/build_wasm_demo.sh
+++ scripts/build_wasm_demo.sh
@@ -6,7 +6,6 @@ cd "$ROOT_DIR"
 
 OUT_DIR="demo/pkg"
 WASM_TARGET_DIR="target/wasm32-unknown-unknown/release"
-PRETEXT_OUT_DIR="demo/vendor/pretext"
 
 cargo build -p mail-canvas-wasm --release --target wasm32-unknown-unknown
 mkdir -p "$OUT_DIR"
@@ -15,9 +14,3 @@ wasm-bindgen \
   --no-typescript \
   --out-dir "$OUT_DIR" \
   "$WASM_TARGET_DIR/mail_canvas_wasm.wasm"
-
-rm -rf "$PRETEXT_OUT_DIR"
-mkdir -p "$PRETEXT_OUT_DIR"
-cp node_modules/@chenglou/pretext/dist/*.js "$PRETEXT_OUT_DIR/"
-mkdir -p "$PRETEXT_OUT_DIR/generated"
-cp node_modules/@chenglou/pretext/dist/generated/*.js "$PRETEXT_OUT_DIR/generated/"
